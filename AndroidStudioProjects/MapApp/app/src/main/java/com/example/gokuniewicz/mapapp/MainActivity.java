160020e26b8c6170bba3de57fc87637231889a5c
g-t-okuniewicz <ted-sites@live.com>
2017-11-03 16:19:56 +0000

    fragments

--- AndroidStudioProjects/MapApp/app/src/main/java/com/example/gokuniewicz/mapapp/MainActivity.java
+++ AndroidStudioProjects/MapApp/app/src/main/java/com/example/gokuniewicz/mapapp/MainActivity.java
@@ -15,20 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
+        //textView = (TextView)findViewById(R.id.textView);
     }
 
-    @Override
+    /*@Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            textView.setText("landscape");
+            //textView.setText("landscape");
             Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            textView.setText("portrait");
+            //textView.setText("portrait");
             Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
         }
-    }
+    }*/
 }
