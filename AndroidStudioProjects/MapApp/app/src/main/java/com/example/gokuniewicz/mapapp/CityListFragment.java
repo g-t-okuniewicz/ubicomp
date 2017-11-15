8b1b81a28e4259961ba35aff11abbc608fa57b7c
g-t-okuniewicz <ted-sites@live.com>
2017-11-15 16:02:42 +0000

    Landscape done

--- AndroidStudioProjects/MapApp/app/src/main/java/com/example/gokuniewicz/mapapp/CityListFragment.java
+++ AndroidStudioProjects/MapApp/app/src/main/java/com/example/gokuniewicz/mapapp/CityListFragment.java
@@ -1,19 +1,22 @@
 package com.example.gokuniewicz.mapapp;
 
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CityListFragment extends Fragment {
+public class CityListFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     String[] cityArray = {
             "Dublin",
@@ -23,6 +26,8 @@ public class CityListFragment extends Fragment {
             "Galway",
             "Wexford"
     };
+
+    ListView listView;
 
     public CityListFragment() {
         // Required empty public constructor
@@ -34,7 +39,8 @@ public class CityListFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_city_list, container, false);
 
-        ListView listView = (ListView)view.findViewById(R.id.cityListView);
+        listView = (ListView)view.findViewById(R.id.cityListView);
+        listView.setOnItemClickListener(this);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getActivity(),
@@ -48,4 +54,16 @@ public class CityListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String location = listView.getItemAtPosition(i).toString();
+
+        Activity activity = getActivity();
+
+        Toast.makeText(activity, "Clicked: " + location, Toast.LENGTH_SHORT).show();
+
+        MapFragment fragment = (MapFragment) activity.getFragmentManager().findFragmentById(R.id.mapFragment);
+
+        fragment.moveMarker(location);
+    }
 }
--- AndroidStudioProjects/MapApp/app/src/main/java/com/example/gokuniewicz/mapapp/MapFragment.java
+++ AndroidStudioProjects/MapApp/app/src/main/java/com/example/gokuniewicz/mapapp/MapFragment.java
@@ -3,7 +3,7 @@ package com.example.gokuniewicz.mapapp;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
@@ -24,11 +25,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     String location = "Dublin";
-    double lat, lon;
+    double lat, lng;
 
     private GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
+
+    Marker mMarker;
+    CameraPosition cameraPosition;
 
     public MapFragment() {
         // Required empty public constructor
@@ -70,38 +74,47 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mGoogleMap = googleMap;
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title(location).snippet(location));
+        mMarker = mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(location).snippet(lat + ", " + lng));
 
-        CameraPosition liberty = CameraPosition.builder().target(new LatLng(lat, lon)).zoom(6).bearing(0).tilt(45).build();
+        cameraPosition = CameraPosition.builder().target(new LatLng(lat, lng)).zoom(6).bearing(0).tilt(45).build();
 
-        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    public void moveMarker(String location) {
+        setCoordinates(location);
+        mMarker.setPosition(new LatLng(lat, lng));
+        mMarker.setTitle(location);
+        mMarker.setSnippet(lat + ", " + lng);
+        CameraPosition cameraPosition = CameraPosition.builder().target(new LatLng(lat, lng)).zoom(6).bearing(0).tilt(45).build();
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
     private void setCoordinates(String location) {
         switch(location) {
             case "Dublin":
                 lat = 53.347860;
-                lon = -6.272487;
+                lng = -6.272487;
                 break;
             case "Kerry":
                 lat = 52.264007;
-                lon = -9.686990;
+                lng = -9.686990;
                 break;
             case "Belfast":
                 lat = 54.602755;
-                lon = -5.945180;
+                lng = -5.945180;
                 break;
             case "Cork":
                 lat = 51.892171;
-                lon = -8.475068;
+                lng = -8.475068;
                 break;
             case "Galway":
                 lat = 53.276533;
-                lon = -9.069362;
+                lng = -9.069362;
                 break;
             case "Wexford":
                 lat = 52.336521;
-                lon = -6.462855;
+                lng = -6.462855;
                 break;
         }
     }
